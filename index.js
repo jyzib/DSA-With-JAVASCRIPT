b94ee88da5e01fa8fd9ecbe0a2424f9c4b1260d4
jyzib <jazibzaidi02@gmail.com>
2023-05-23 09:05:16 +0530

    sort 0 1 using two pointer

--- index.js
+++ index.js
@@ -513,22 +513,58 @@
 
 // Find the tepleat of the sum with K
 
-function findtepleat(n,r){
-      for(let i = 0;i<n.length;i++){
-         for(let j = i + 1 ; j<n.length;j++){
-           for(let k = j + 1 ;k<n.length;k++){
-            if(n[i]+n[j]+n[k] == r ){
-              console.log(n[i],n[j],n[k])
-              break
-            }
-           }
-         }
+// function findtepleat(n,r){
+//       for(let i = 0;i<n.length;i++){
+//          for(let j = i + 1 ; j<n.length;j++){
+//            for(let k = j + 1 ;k<n.length;k++){
+//             if(n[i]+n[j]+n[k] == r ){
+//               console.log(n[i],n[j],n[k])
+//               break
+//             }
+//            }
+//          }
      
 
-      }
+//       }
+// }
+// let n = [1,2,3,4,5]
+// findtepleat(n,9)
+// sort an array of 0 1 with sort methord
+// let arr = [0,1,0,1,1,1,0,0,0,0]
+// console.log(arr.sort())
+
+// solving this with two pointer aprouch
+
+let arr = [0,1,0,1,1,1,0,0,0,0]
+let i = 0
+let j = arr.length -1 
+ while(i<j){
+  while(arr[i] == 0 && i< j ){
+    
+    i++
+  }
+  while(arr[j]==1&& i<j){
+    j--
+  }
+  if(arr[i]==1&&arr[j]==0&&i<j){
+
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+ 
 }
-let n = [1,2,3,4,5]
-findtepleat(n,9)
+
+console.log(arr)
+
+
+
+
+
+
+
+
+
 
 
 
